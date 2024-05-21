a3a051b856f029c2ce6b7f69ddb82fd3abe40fc6
r0n4ldCB <35352167+r0n4ldCB@users.noreply.github.com>
2024-05-21 01:31:40 -0600

    cambios responsive
    cambios minimos

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ import ExpenseChart from "./components/transactions/ExpenseChart";
 function App() {
   return (
     <GlobalProvider>
-      <div className="bg-zinc-900 text-white h-screen w-screen flex justify-center items-center">
+      <div className="bg-zinc-900 text-white w-screen flex justify-center items-center sm:flex-wrap md:flex-wrap">
         <div className="container mx-auto w-3/6">
-          <div className="bg-slate-800 p-10 rounded-lg flex gap-x-2">
+          <div className="bg-slate-800 p-10 rounded-lg flex sm:justify-center sm:flex-wrap md:flex-wrap gap-x-2">
             <div>
               <h1 className="text-4xl font-bold">Contabilizador</h1>
               <IncomeExpenses />
